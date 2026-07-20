933eef422b1eb0bb3795ceea8c071baa13099b68
helenclarko <helenclarko@users.noreply.github.com>
2026-07-20 13:46:28 +0000

    preview: local-development-jumpgate @ 7d5a4610aee0c75e71093c0d670f9818d8bcbbc1 7d5a4610aee0c75e71093c0d670f9818d8bcbbc1

---
+++ branches/local-development-jumpgate/betteR20-core.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         betteR20-beta-core-death-jumpagate-import
+// @namespace    https://5e.tools/
+// @license      MIT (https://opensource.org/licenses/MIT)
+// @version      1.36.1.7-beta-preview-local-development-jumpgate
+// @updateURL    https://mighty-frosty.github.io/betterR20/branches/local-development-jumpgate/betteR20-core.meta.js
+// @downloadURL  https://mighty-frosty.github.io/betterR20/branches/local-development-jumpgate/betteR20-core.user.js
+// @description  Enhance your Roll20 experience
+// @author       TheGiddyLimit/Redweller
+
+// @match        https://app.roll20.net/editor
+// @match        https://app.roll20.net/editor#*
+// @match        https://app.roll20.net/editor?*
+// @match        https://app.roll20.net/editor/
+// @match        https://app.roll20.net/editor/#*
+// @match        https://app.roll20.net/editor/?*
+
+// @grant        unsafeWindow
+// @run-at       document-start
+
+// @grant        GM_webRequest
+// @webRequest   [{"selector": { "include": "*://www.google-analytics.com/analytics.js" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://cdn.userleap.com/shim.js?*" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://analytics.tiktok.com/*" },  "action": "cancel"}]
+
+// ==/UserScript==
