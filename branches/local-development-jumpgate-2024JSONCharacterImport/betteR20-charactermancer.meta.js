254ea1cd2afc20dcae20843949bf87246613b9c6
helenclarko <helenclarko@users.noreply.github.com>
2026-08-19 01:51:38 +0000

    preview: local-development-jumpgate-2024JSONCharacterImport @ 5931ff311f671bbea4274f85e7e35f7731e852ab 5931ff311f671bbea4274f85e7e35f7731e852ab

---
+++ branches/local-development-jumpgate-2024JSONCharacterImport/betteR20-charactermancer.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         betteR20-beta-charactermancer-death-jumpagate-import
+// @namespace    https://5e.tools/
+// @license      MIT (https://opensource.org/licenses/MIT)
+// @version      1.36.1.7-beta-preview-local-development-jumpgate-2024JSONCharacterImport
+// @updateURL    https://mighty-frosty.github.io/betterR20/branches/local-development-jumpgate-2024JSONCharacterImport/betteR20-charactermancer.meta.js
+// @downloadURL  https://mighty-frosty.github.io/betterR20/branches/local-development-jumpgate-2024JSONCharacterImport/betteR20-charactermancer.user.js
+// @description  Charactermancer for betteR20 (can be disabled independently in Tampermonkey)
+// @author       5egmegaanon/astranauta/MrLabRat/TheGiddyLimit/DBAWiseMan/BDeveau/Remuz/Callador Julaan/Erogroth/Stormy/FlayedOne/Cucucc/Cee/oldewyrm/darthbeep/Mertang/Redweller/DeathStalker
+
+// @match        https://app.roll20.net/editor
+// @match        https://app.roll20.net/editor#*
+// @match        https://app.roll20.net/editor?*
+// @match        https://app.roll20.net/editor/
+// @match        https://app.roll20.net/editor/#*
+// @match        https://app.roll20.net/editor/?*
+// @match        https://*.roll20preflight.net/*
+
+// @grant        unsafeWindow
+// @run-at       document-start
+// ==/UserScript==
