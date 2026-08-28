b2e341e641bab89add68850a59cdd846d9f891f2
helenclarko <helenclarko@users.noreply.github.com>
2026-08-28 15:02:23 +0000

    preview: local-development-jumpgate-OGLClasses @ 54135661204b4f4a549fe602ba2f6714df5254c9 54135661204b4f4a549fe602ba2f6714df5254c9

---
+++ branches/local-development-jumpgate-OGLClasses/betteR20-5etools.meta.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         betteR20-beta-5etools-death-jumpagate-import
+// @namespace    https://5e.tools/
+// @license      MIT (https://opensource.org/licenses/MIT)
+// @version      1.36.1.8-beta-preview-local-development-jumpgate-OGLClasses
+// @updateURL    https://mighty-frosty.github.io/betterR20/branches/local-development-jumpgate-OGLClasses/betteR20-5etools.meta.js
+// @downloadURL  https://mighty-frosty.github.io/betterR20/branches/local-development-jumpgate-OGLClasses/betteR20-5etools.user.js
+// @description  Enhance your Roll20 experience
+// @author       5egmegaanon/astranauta/MrLabRat/TheGiddyLimit/DBAWiseMan/BDeveau/Remuz/Callador Julaan/Erogroth/Stormy/FlayedOne/Cucucc/Cee/oldewyrm/darthbeep/Mertang/Redweller/DeathStalker
+
+// @match        https://app.roll20.net/editor
+// @match        https://app.roll20.net/editor#*
+// @match        https://app.roll20.net/editor?*
+// @match        https://app.roll20.net/editor/
+// @match        https://app.roll20.net/editor/#*
+// @match        https://app.roll20.net/editor/?*
+// @match        https://*.roll20preflight.net/*
+
+// @grant        unsafeWindow
+// @run-at       document-start
+
+// @grant        GM_webRequest
+// @webRequest   [{"selector": { "include": "*://www.google-analytics.com/analytics.js" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://cdn.userleap.com/shim.js?*" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://analytics.tiktok.com/*" },  "action": "cancel"}]
+
+// ==/UserScript==
